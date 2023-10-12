7e56955818d4c3f018e5a7ec9109deb22cf6fb6f
gulya <gulya@STEPAN>
2023-10-12 15:55:26 +0700

    Добавьте файлы проекта.

---
+++ laba3_4/laba3_4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Введите вашу оценку: ";
+    int mark;
+    cin >> mark;
+    switch (mark) {
+        case 5: 
+            cout << "Отлично!";
+            break;
+        case 4:
+            cout << "Хорошо!";
+            break;
+        case 3:
+            cout << "Средне!";
+            break;
+        case 2:
+            cout << "Хуже среднего!";
+            break;
+        case 1:
+            cout << "Хуже среднего!";
+            break;
+        case 0:
+            cout << "Хуже среднего!";
+            break;
+        default:
+            cout << "Оценка вне диапозона";
+
+     
+    }
+}
